c91c670848b152fe65fed08c3c66297e402d41aa
Michael <michaelscuteridev@gmail.com>
2024-08-08 23:02:10 +1000

    Added multi threading

--- Tools/PortScanner.py
+++ Tools/PortScanner.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import threading
+from datetime import datetime
 
 common_ports = {
     21: "FTP",
@@ -9,26 +11,22 @@ common_ports = {
     53: "DNS",
     80: "HTTP",
     88: "Kerberos",
-    135: "RPC",
     137: "NetBIOS",
     139: "SMB",
     143: "IMAP",
+    161: "SNMP",
+    162: "SNMP",
+    194: "IRC",
     389: "LDAP",
     443: "HTTPS",
     445: "SMB"
 }
 
 if len(sys.argv) < 2:
-    print(f"Usage: PortScanner.py <host/IP> <-quick or -full>")
+    print(f"Usage: PortScanner.py <host/IP>")
     sys.exit(1)
 
 host = sys.argv[1]
-scan_type = sys.argv[2]
-
-if sys.argv[2] == "-quick":
-    port_limit = 1001
-elif sys.argv[2] == "-full":
-    port_limit = 65536
 
 try:
     hostIP = socket.gethostbyname(host)
@@ -38,26 +36,45 @@ except socket.gaierror:
 
 socket.setdefaulttimeout(0.1)
 
-print("-" * 60)
-print(f"Scanning host at {hostIP}")
-print("-" * 60)
+print("-" * 35)
+print(f"| Scanning host at {hostIP}  |")
+print("-" * 35)
 
-try:
-    for port in range(1, port_limit):
-        sys.stdout.write(f"\rScanning port {port}/{port_limit}")
-        sys.stdout.flush()
+t1 = datetime.now()
+
+def scan_port(port):
+    try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = s.connect_ex((hostIP, port))
         if result == 0:
             if port in common_ports:
-                print(f"\r{' ' * 60}\rPort {port}({common_ports[port]}): Open")
+                print(f"Port {port}({common_ports[port]}): Open")
             else:
-                print(f"\r{' ' * 60}\rPort {port}: Open")
+                print(f"Port {port}: Open")
         s.close()
+
+    except socket.error as e:
+        print(f"Socket error: {e}")
+
+threads = []
+
+try:
+    for port in range(1, 65536):
+        t = threading.Thread(target=scan_port, args=(port,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    t2 = datetime.now()
+    total_time = t2 - t1
 
     sys.stdout.write("\r" + " " * 30 + "\r")
     sys.stdout.flush()
-    print("\nScan Completed")
+    print("-"*36)
+    print(f"| Scan Completed in {total_time} |")
+    print("-"*36)
 
 except KeyboardInterrupt:
     sys.exit(1)
